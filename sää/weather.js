324cbea7f34e6b9fa5cac10e39076aea2dd38ae0
sipsuli <silja.angervuori@gmail.com>
2021-03-07 19:04:04 +0200

    weather

--- sää/weather.js
+++ sää/weather.js
@@ -28,28 +28,45 @@ function getWeather () {
     fetch(api)
     .then(function(response){
         let data = response.json();
-        console.log(data);
-        console.log(api);
         return data;
     })
     .then(function(data){
         weather.tila = data.list[0].main.temp-273.15;
         weather.iconId = data.list[0].weather[0].icon;
-        console.log(weather.iconId);
+        weather.list = data.list;
+        var mainContainer = document.getElementsByClassName("day");
+        for(var i=0; i<weather.list.length; i++){
+            var everyNinth = (i+1) % 8 === 0;
+            if (everyNinth) {
+            console.log(data.list[i].main.temp-273.15);
+            console.log(data.list[i].weather[0].icon);
+        }
+            var weathers = document.createElement("li");
+        var text = "";
+        var x;
+
+        }   
+         //         menu.menus = data.courses;
+//         var text = "";
+//         var x;
+//         var mainContainer = document.getElementById("menus");
+            //         for (x in data.courses){
+//             var dish = document.createElement("li");
+//             text += data.courses[x].title_fi + data.courses[x].price +
+//             data.courses[x].dietcodes+"<br>";
+//             dish.innerHTML = text;
+            // var everyninth = (i+1) % 9 === 0;
+
     })
     .then(function(){
         displayWeather();
     })
     function displayWeather() {
-        
-                const place = document.querySelector(".degree");
-                place.innerHTML = weather.tila;
-                const iconElement = document.querySelector("description");
-                let icon = document.createElement("img");
-                iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.iconId}.png"/>`;
-                
+        const place = document.querySelector(".degree");
+        place.innerHTML = weather.tila.toFixed(1) + " C";
+        const iconElement = document.querySelector("description");
+        iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/04n.png"/>`;        
             }
-            iconElement.appendChild(icon);
 }
 
 
@@ -88,3 +105,32 @@ function getWeather () {
 //         place.innerHTML = menu.place;
 //     }
 // }
+function getDay() {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var container = document.getElementById("demo");
+    
+    for(i=0; i<5; i++){
+        var n = d.getDay()+1; 
+        console.log(weekday[d.getDay()+n+i]);
+        var weekdays = weekday[d.getDay()+n+i];
+        var day = document.createElement("li");
+       
+       container.appendChild(day);
+    }
+    
+    day.innerHTML = weekdays;
+  }
+
+  function allFunctions() {
+    getDay();
+    getWeather();
+  }
